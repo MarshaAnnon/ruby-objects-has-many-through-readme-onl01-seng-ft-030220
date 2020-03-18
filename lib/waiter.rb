90c31f4367b85a2e37710564d2bd23feeaf10924
Marsha Annon <marshaannon@gmail.com>
2020-03-18 17:39:25 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -3,6 +3,7 @@ class Waiter
   attr_accessor :name, :yrs_experience
 
   @@all = []
+  
   def initialize(name, @yrs_experience)
     @name = name
     @yrs_experience = yrs_experience
@@ -16,5 +17,5 @@ class Waiter
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
-  
+
 end
